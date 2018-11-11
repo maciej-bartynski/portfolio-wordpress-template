3e9b2207fcb61e31b11bd5995c32750b2da9ba8e
JestemBartek <maciek.bartynski@gmail.com>
2018-11-11 16:28:20 +0100

    search overlay

--- bluish/src/scripts/search-panel.js
+++ bluish/src/scripts/search-panel.js
@@ -1,24 +1,28 @@
 import jquery from 'jquery';
-
+import zenscroll from 'zenscroll';
 class SearchPanel {
     constructor() {
         this.panel = jquery ('#search-panel01-b');
         this.searchIcon = jquery('#search-icon');
         this.closeIcon = jquery('#close-search-icon');
         this.searchMode=false;
+        this.timer;
         this.clickDispatcher();
     }
     clickDispatcher(){
         this.searchIcon.click(
             ()=>{
+                zenscroll.toY(0, 0)
                 this.panel.fadeIn();
                 this.searchMode=true;
+                //this.timer = setTimeout(()=>{zenscroll.toY(0, 0)}, 800);
             }
         );
         this.closeIcon.click(
             ()=>{
                 this.panel.fadeOut();
                 this.searchMode=false;
+                //clearTimeout(this.timer);
             }
         );
     }
